afbeb965679d019c590bdf6180094825ba5531ca
aasas231 <3194702783@qq.com>
2026-04-17 15:50:57 +0800

    Add SurVis authorized tags

---
+++ cw3/data/authorized_tags.js
@@ -0,0 +1,7 @@
+const userDefinedAuthorizedTags = {
+    "category:morphology_for_rpm": "Morphology-focused RPM augmentation",
+    "category:rpm_architectures": "RPM solver architectures",
+    "category:rpm_benchmark_extension": "RPM benchmark design and extensions",
+    "category:learned_augmentation": "Automated augmentation policy learning",
+    "category:patch_mixing_augmentation": "Patch/mixing augmentation"
+};
